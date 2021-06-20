0f05cad94eb250ec97adf585fe734a1422cd18fd
dingkan <657483726@qq.com>
2021-06-20 09:50:35 +0800

    brenv统一设置Ruby环境。RubyGems统一安装管理cocapods&fastlane

---
+++ DKNormalProject/Shared/DKNormalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DKNormalProjectApp.swift
+//  Shared
+//
+//  Created by 丁侃 on 2021/6/19.
+//
+
+import SwiftUI
+
+@main
+struct DKNormalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
